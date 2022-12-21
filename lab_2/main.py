d6b809264b5db976448cace0e6d34430625c0e39
onlyonenastya <91826517+onlyonenastya@users.noreply.github.com>
2022-12-21 23:51:04 +0300

    Add files via upload

---
+++ lab_2/main.py
@@ -0,0 +1,18 @@
+from lab_python_oop.rectangle import Rectangle
+from lab_python_oop.circle import Circle
+from lab_python_oop.square import Square
+import cowsay
+
+
+def main():
+
+    r = Rectangle("синего", 3, 2)
+    c = Circle("зеленого", 5)
+    s = Square("красного", 5)
+    print(r)
+    print(c)
+    print(s)
+    cowsay.stegosaurus(str(r) + '\n' + str(c) + '\n' + str(s))
+
+if __name__ == "__main__":
+    main()
